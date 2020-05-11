7cb34ffccc8ce695de6576153b9ec7e348fc250a
patosmolar <patosmolar@gmail.com>
2020-05-11 13:52:02 +0200

    update

--- bakalarka/routes.py
+++ bakalarka/routes.py
@@ -1,9 +1,10 @@
 import requests,json,sys,time
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request,jsonify
 from bakalarka import app, db, bcrypt
 from bakalarka.forms import  LoginForm
 from bakalarka.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+import json
 
 
 @app.context_processor
@@ -33,13 +34,11 @@ def home():
 
 
 
-@app.route("/about")
+@app.route("/scheduler")
 @login_required
-def about():
-    return render_template('about.html', title='About')
+def scheduler():
+    return render_template('scheduler.html', title='Scheduler')
   
-
-
 
 @app.route("/login",  methods=['GET', 'POST'])
 def login():
@@ -67,3 +66,19 @@ def logout():
 @login_required
 def account():
     return render_template('account.html', title='Account')
+
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+    # You'd normally use the variables above to limit the data returned
+    # you don't want to return ALL events like in this code
+    # but since no db or any real storage is implemented I'm just
+    # returning data from a text file that contains json elements
+
+    with open("bakalarka/events.json", "r") as input_data:
+        # you should use something else here than just plaintext
+        # check out jsonfiy method or the built in json module
+        # http://flask.pocoo.org/docs/0.10/api/#module-flask.json
+        return input_data.read()
